3179d7472ef8feeea2a6da0a54a74a9fccc6bce4
wawss <first_qq_email@qq.com>
2018-03-13 23:20:20 +0800

    产品组件

--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -9,7 +9,7 @@ import {
     Dimensions,
     TouchableHighlight
 } from 'react-native';
-import {deviceWidth, deviceHeight,getSizeOfImage} from "../tools/ScreenUtil";
+import {deviceWidth, deviceHeight, getSizeOfImage, setSpText} from "../tools/ScreenUtil";
 export default class Cakes extends React.Component {
     constructor(props) {
         super(props);
@@ -20,22 +20,44 @@ export default class Cakes extends React.Component {
             dataSource: ds.cloneWithRows([
                 {
                     imageUri: require('../images/p-test.png'),
-                    name: '迪士尼蛋糕'
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
                 }, {
                     imageUri: require('../images/p-test.png'),
-                    name: '迪士尼蛋糕2'
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
                 }, {
                     imageUri: require('../images/p-test.png'),
-                    name: '迪士尼蛋糕2'
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
                 }, {
                     imageUri: require('../images/p-test.png'),
-                    name: '迪士尼蛋糕2'
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
                 }, {
                     imageUri: require('../images/p-test.png'),
-                    name: '迪士尼蛋糕2'
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
                 }, {
                     imageUri: require('../images/p-test.png'),
-                    name: '迪士尼蛋糕2'
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
+                }, {
+                    imageUri: require('../images/p-test.png'),
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
+                }, {
+                    imageUri: require('../images/p-test.png'),
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
+                }, {
+                    imageUri: require('../images/p-test.png'),
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
+                }, {
+                    imageUri: require('../images/p-test.png'),
+                    name: '迪士尼蛋糕',
+                    english: 'dincakedd'
                 }
             ])
         };
@@ -47,50 +69,93 @@ export default class Cakes extends React.Component {
         this.renderRow = (rowData, sid, rid) => {
             return (
                 <TouchableHighlight underlayColor='red'>
-                <View style={[styles.item,this.itemStyle(rid)]}>
-                    <Image source={rowData.imageUri} style={styles.itemImg}/>
-                    <Text style={styles.itemName}>{rowData.name}</Text>
-                </View>
+                    <View
+                        style={[
+                        styles.item, this.itemStyle(rid)
+                    ]}>
+                        <Image source={rowData.imageUri} style={styles.itemImg}/>
+                        <View style={styles.viewForItemName}>
+                            <Text style={styles.styleForText}>{rowData.name}</Text>
+                            <Text style={styles.styleForText}>{rowData.english}</Text>
+                        </View>
+                    </View>
                 </TouchableHighlight>
             );
         };
     }
     render() {
         return (
-            <ListView
-                contentContainerStyle={styles.ListView}
-                dataSource={this.state.dataSource}
-                renderRow={this.renderRow}></ListView>
+            <View style={styles.container}>
+                 <View style={styles.titleBox}>
+                     <Text style={styles.titleDesc}>全&nbsp;部&nbsp;产&nbsp;品</Text>
+                     <Text style={styles.engTitleDesc}>All Products</Text>
+                 </View>
+                <ListView
+                    contentContainerStyle={styles.ListView}
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow}></ListView>
+            </View>
         );
     };
 
 }
 var {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        
+    },
+    titleBox:{
+        marginLeft:(deviceWidth * 0.06),
+        marginRight:(deviceWidth * 0.06),
+        marginTop:(deviceWidth * 0.06),
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        alignItems: 'flex-end',
+        borderBottomWidth:1,
+        borderBottomColor:'#7bc2d5',
+    },
+    titleDesc:{
+        color:'#686868',
+        fontSize: setSpText(15)
+    },
+    engTitleDesc:{
+        color:'#686868',
+        fontSize: setSpText(12)
+    },
     ListView: {
-        'display': 'flex',
-        'flexDirection': 'row',
+        display: 'flex',
+        flexDirection: 'row',
         flexWrap: 'wrap',
-        margin: 0,
-        paddingLeft:deviceWidth*0.06,
+        paddingLeft:(deviceWidth * 0.06),
+        paddingRight:(deviceWidth * 0.06),
+        paddingBottom:(deviceWidth * 0.5),
+        justifyContent: 'center',
     },
-    item:{
-        width: deviceWidth*0.42,
-        height:  deviceWidth*0.42,
-        marginBottom: deviceWidth*0.1,
+    item: {
+        'display': 'flex',
+        backgroundColor:'white',
+        width: (deviceWidth * 0.44),
+        justifyContent: 'center',
+        alignItems:'center',
+        paddingTop: (deviceWidth * 0.04),
     },
-    itemName:{
-
+    viewForItemName: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    styleForText: {
+        color: '#686868',
+        fontSize: setSpText(16)
     },
     itemOdd: {
-        marginRight:deviceWidth*0.02
+        // marginRight: (deviceWidth * 0.02)
     },
     itemEven: {
-        marginLeft:deviceWidth*0.02
+        // marginLeft: (deviceWidth * 0.02)
     },
     itemImg: {
-        width: deviceWidth*0.42,
-        height:  deviceWidth*0.42
+        width: (deviceWidth * 0.40),
+        height: (deviceWidth * 0.40)
     }
 });
